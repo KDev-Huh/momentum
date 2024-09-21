e66badc084c05c825e6f1c632c240b307ddd6b94
huhonmcmc <huhonmcmc@gmail.com>
2024-09-21 10:39:29 +0900

    사진 경로 변경

--- js/background.js
+++ js/background.js
@@ -9,7 +9,7 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const container = document.getElementById("container");
 
-container.style.backgroundImage = `url('../img/${chosenImage}')`;
+container.style.backgroundImage = `url('./img/${chosenImage}')`;
 
 // const bgImage = document.createElement("img");
 // bgImage.src = `../img/${chosenImage}`;
